02c93c0101b83fe89091dcf50f562a0ece17c5ee
William Sercombe <wsercombe@gmail.com>
2021-09-15 16:39:53 -0700

    Test for purchasing 2 loaves

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -12,6 +12,13 @@ namespace Bakery.Tests
       int price = breadTest.Price;
       Assert.AreEqual(5, price);
     }
+    [TestMethod]
+    public void TwoLoavesForTenDollars() {
+      Bread breadTest = new Bread("2");
+      int price = breadTest.Price;
+      Assert.AreEqual(10, price);
+    }
+
     // Test method will go here
   }
 }
